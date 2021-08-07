de9d255d2099dd0367e7cb9c203e6589d3a9e00a
bosetridib <bosetridib@yahoo.com>
2021-08-07 20:17:29 +0530

    ...

--- ModelSimulation.R
+++ ModelSimulation.R
@@ -1,4 +1,15 @@
-# ================== The simulated time-series ================== #
+# The Weierstrass function returning values with a,b, with wf_iteration terms
+wf <- function(x,n,a,b)
+{
+  v <- NULL
+  for(j in 1:n)
+  {
+    v[j] <- (a^j)*cos((b^j)*x)    # the formula of each term in Weierstrass function
+  }
+  return(sum(v))
+}
+
+# ================== 1. The simulated AR(0) time-series ================== #
 
 y <- NULL    # The generated volatile variable
 
@@ -14,17 +25,6 @@ u <- rnorm(n,0,std_dev)
 
 # The index of each observation, acting as time series variable 't'
 x <- 1:n
-
-# The Weierstrass function returning values with a,b, with wf_iteration terms
-wf <- function(x,n,a,b)
-{
-  v <- NULL
-  for(j in 1:n)
-  {
-    v[j] <- (a^j)*cos((b^j)*x)    # the formula of each term in Weierstrass function
-  }
-  return(sum(v))
-}
 
 # The time-series variable with wf+u values
 
@@ -75,3 +75,173 @@ list(
       var.test(y[q13],y[q34]),
       var.test(y[q01],y[q34])
     )
+
+
+
+
+
+
+
+
+
+
+# ============= 2. The simulated time-series with a upward trend ============= #
+
+y_trend <- NULL
+a <- 1.1
+b <- 17
+wf_iteration_trend <- 41
+stdev <- 40
+n <- 200
+u <- rnorm(n,0,stdev)
+x <- 1:n
+trend_coefficient <- 10*x^(0.75)
+
+# The TS . . .
+
+for(i in 1:n)
+{
+  if((i <= n/4) || (i > 3*n/4))
+  {
+    y_trend[i] <- trend_coefficient[i]+wf(x[i],1,a,b)+u[i]
+  }
+  
+  else
+  {
+    y_trend[i] <- trend_coefficient[i]+wf(x[i],wf_iteration_trend,a,b)+u[i]
+  }
+}
+
+# The trend . . .
+
+at <- NULL
+
+for(i in 1:n)
+{
+  if((i <= n/4) || (i > 3*n/4))
+  {
+    at[i] <- trend_coefficient[i]+wf(x[i],1,a,b)
+  }
+  
+  else
+  {
+    at[i] <- trend_coefficient[i]+wf(x[i],wf_iteration_trend,a,b)
+  }
+}
+
+
+plot(x, y_trend, type="l")
+# plot(x, at, type="l")
+# plot(x[q13], at[q13], type="l")
+lines(x,at,col="brown")
+
+q01 <- 1:(n/4)
+q13 <- ((n/4)+1):(3*n/4)
+q34 <- (3*n/4+1):n
+list(
+  var.test(y_trend[q01],y_trend[q13]),
+  var.test(y_trend[q13],y_trend[q34]),
+  var.test(y_trend[q01],y_trend[q34])
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ================== 3. The simulated AR(1) time-series ================== #
+
+y_ar1 <- NULL
+
+# The parameters of the Weierstrass function
+a <- 1.1
+b <- 17
+
+# The number of terms in the function
+wf_iteration_ar1 <- 31
+
+# The IIDN residual observations
+stdev <- 40
+n <- 200
+u <- rnorm(n,0,stdev)
+x <- 1:n
+
+# The AR(1) coefficient
+rho <- 1
+
+# The AR(1) time-series
+
+for(i in 1:n)
+{
+  if(i == 1)
+  {
+    y_ar1[i] <- wf(x[i],1,a,b) + u[i]
+  }
+  else
+  {
+    if((i <= n/4) || (i > 3*n/4))                   # the stable period of the first and last quarters
+    {
+      y_ar1[i] <- wf(x[i],1,a,b) +                  # for stability, wf_iteration_ar1 is 1
+        rho*y_ar1[i-1] + u[i]
+    }
+    
+    else                                            # the volatile periods in the second and third quarters
+    {
+      y_ar1[i] <- wf(x[i],wf_iteration_ar1,a,b) +   # for volatility, wf_iteration_ar1 is stated above
+        rho*y_ar1[i-1] + u[i]
+    }
+  }
+}
+
+# The trend of the AR(1) model, basically the y_ar1 without the residuals
+
+trend_ar1 <- NULL
+
+for(i in 1:n)
+{
+  if(i == 1)
+  {
+    trend_ar1[i] <- wf(x[i],1,a,b)
+  }
+  else
+  {
+    if((i <= n/4) || (i > 3*n/4))
+    {
+      trend_ar1[i] <- wf(x[i],1,a,b) +
+        rho*trend_ar1[i-1]
+    }
+    
+    else
+    {
+      trend_ar1[i] <- wf(x[i],wf_iteration_ar1,a,b) +
+        rho*trend_ar1[i-1]
+    }
+  }
+}
+
+# The plots illustrates the time-series and its autoregressive trend
+                  
+plot(x,y_ar1,type="l",                                # the plot of the time-series
+     ylim = c(min(min(trend_ar1), min(y_ar1))-stdev,
+              max(max(trend_ar1), max(y_ar1))+stdev)
+)
+lines(x,trend_ar1,col="red")                          # the plot of the trend
+
+# Variance tests of the differenced for of the time-series within the volatile vs stable periods
+
+q01 <- 1:(n/4)
+q13 <- ((n/4)+1):(3*n/4)
+q34 <- (3*n/4+1):n
+list(
+  var.test(diff(y_ar1[q01]),diff(y_ar1[q13])),
+  var.test(diff(y_ar1[q13]),diff(y_ar1[q34])),
+  var.test(diff(y_ar1[q01]),diff(y_ar1[q34]))
+)
--- README.md
+++ README.md
@@ -8,10 +8,24 @@ Nothing except R (https://www.r-project.org/) is currently required to run this 
 
 I used RStudio IDE (https://www.rstudio.com/products/rstudio/download/#download) which can be downloaded from their official website or from any available linux package managers.
 
-## The models
+## The Project and the Idea
 
-### Model-1
+That volatility can be explained by the trend and not the variance, is a novel idea. But to illustrate, and even to successfully execute the testing of this idea requires simulations. As the simulations done reflects, at least partially, the real-world data, we go further and re-estimate the parameters of the simulated models so as to establish the applicable methods.
 
-In this model, we simulate volatility with the Weierstrass function. While the function is a sum of infinite series, the number of terms we use in the series is specified with wfi_iteration term. The objective of this part is to illustrate the idea with simulated stable and volatile periods, in which the trend of the time series fluctuates, rather than the variance of the residuals.
+In lucid economic terms, one might be suggested that apart from modelling the variance of the differenced form of the data, one may consider to model the trend which would would be more intuitive than the general definitions. If one argues that the fluctuations in the data is not just _noise_ but also a fluctuation of _habit/behavior_ of the variable, then one might come to a better understanding of the economic interpretation behind the data.
+
+## The Models
+
+### ModelSimulation
+
+In this model, we simulate volatility with the Weierstrass function. While the function is a sum of infinite series, the number of terms we use in the series is specified with wfi_iteration term. The objective of this part is to illustrate the _idea_ with simulated stable and volatile periods, in which the trend of the time series fluctuates, rather than the variance of the residuals.
 
 The user may change the values of a, b, n, std_dev and wfi_iteration to see the result of different combinations of the parameters generating processes which ranges from seeming purely mathematical to closer to real-world time-series data.
+
+In the first part, the plot would show the differenced form of the time-series with this idea, while in the second part, the plot shows the AR(1) form of the time series. The trend is included in both parts to illustrate the idea.
+
+![alt text]()
+
+In the second part, the plot would show the AR(1) form of the time-series with the same idea. The objective here is to show that the AR(1) trend is stable, and after the first difference, the variance-tests shows the same result as the previous one.
+
+![alt text]()

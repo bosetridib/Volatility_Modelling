6320ccf5241ab3e04a2405eeb201d5964f55c09b
bosetridib <bosetridib@yahoo.com>
2021-08-10 23:51:18 +0530

    ...

--- ModelSimulation.R
+++ ModelSimulation.R
@@ -9,10 +9,6 @@ wf <- function(x,n,a,b)
   return(sum(v))
 }
 
-# ================== 1. The simulated AR(0) time-series ================== #
-
-y <- NULL    # The generated volatile variable
-
 # The required parameters in Weierstrass function.
 a <- 1.5               # this parameter's for amplitude
 b <- 5                 # this parameter's for the fluctuations
@@ -20,11 +16,22 @@ wf_iteration <- 11     # the iteration is the number of terms in the function's 
 
 # The residual with ~IIDN(mean=0,sd)
 std_dev <- 35   # standard deviation
-n <- 100        # number of observations
+n <- 200        # number of observations
 u <- rnorm(n,0,std_dev)
 
 # The index of each observation, acting as time series variable 't'
 x <- 1:n
+
+
+
+
+
+
+
+
+# ================== 1. The simulated AR(0) time-series ================== #
+
+y <- NULL    # The generated volatile variable
 
 # The time-series variable with wf+u values
 
@@ -60,11 +67,18 @@ for(i in 1:n)
 
 
 plot(x, y, type="l",lty=1,xlab = "t")       # the stable and volatile periods shown
-lines(x,trend,lty=2,lwd=1)                  # the trend is the dashed line
+lines(x,trend,lty=2,lwd=1, col="blue")                  # the trend is the dashed line
 # plot(x, trend, type="l")                  # the plot of trend only
 # plot(u,type="l")                          # plot of the random normal u
 
-# the three quarters in the stable and volatile periods.
+
+
+
+
+
+
+
+# The three quarters in the stable and volatile periods.
 q01 <- 1:(n/4)
 q13 <- ((n/4)+1):(3*n/4)
 q34 <- (3*n/4+1):n
@@ -88,17 +102,11 @@ list(
 # ============= 2. The simulated time-series with a upward trend ============= #
 
 y_trend <- NULL
-a <- 1.1
-b <- 17
-wf_iteration_trend <- 41
-stdev <- 40
-n <- 200
-u <- rnorm(n,0,stdev)
-x <- 1:n
-trend_coefficient <- 10*x^(0.75)
 
-# The TS . . .
+# The equation of the upward trend
+trend_coefficient <- (1)*x^(1)
 
+# The time-series generation
 for(i in 1:n)
 {
   if((i <= n/4) || (i > 3*n/4))
@@ -108,36 +116,45 @@ for(i in 1:n)
   
   else
   {
-    y_trend[i] <- trend_coefficient[i]+wf(x[i],wf_iteration_trend,a,b)+u[i]
+    y_trend[i] <- trend_coefficient[i]+wf(x[i],wf_iteration,a,b)+u[i]
   }
 }
 
-# The trend . . .
+# The trend generation (the upward trend is included in this)
 
-at <- NULL
+trend_trend <- NULL
 
 for(i in 1:n)
 {
   if((i <= n/4) || (i > 3*n/4))
   {
-    at[i] <- trend_coefficient[i]+wf(x[i],1,a,b)
+    trend_trend[i] <- trend_coefficient[i] +
+      wf(x[i],1,a,b)
   }
   
   else
   {
-    at[i] <- trend_coefficient[i]+wf(x[i],wf_iteration_trend,a,b)
+    trend_trend[i] <- trend_coefficient[i] +
+      wf(x[i],wf_iteration,a,b)
   }
 }
 
 
-plot(x, y_trend, type="l")
-# plot(x, at, type="l")
-# plot(x[q13], at[q13], type="l")
-lines(x,at,col="brown")
+plot(x, y_trend, type="l",xlab = 't')
+lines(x,trend_trend,lty=2,col="brown")
+# plot(x, trend_trend, type="l")
+# plot(x[q13], trend_trend[q13], type="l")
+
+
+
+
+
+
 
 q01 <- 1:(n/4)
 q13 <- ((n/4)+1):(3*n/4)
 q34 <- (3*n/4+1):n
+
 list(
   var.test(y_trend[q01],y_trend[q13]),
   var.test(y_trend[q13],y_trend[q34]),
@@ -151,22 +168,11 @@ list(
 
 
 
+
+
 # ================== 3. The simulated AR(1) time-series ================== #
 
 y_ar1 <- NULL
-
-# The parameters of the Weierstrass function
-a <- 1.1
-b <- 17
-
-# The number of terms in the function
-wf_iteration_ar1 <- 31
-
-# The IIDN residual observations
-stdev <- 40
-n <- 200
-u <- rnorm(n,0,stdev)
-x <- 1:n
 
 # The AR(1) coefficient
 rho <- 1
@@ -189,7 +195,7 @@ for(i in 1:n)
     
     else                                            # the volatile periods in the second and third quarters
     {
-      y_ar1[i] <- wf(x[i],wf_iteration_ar1,a,b) +   # for volatility, wf_iteration_ar1 is stated above
+      y_ar1[i] <- wf(x[i],wf_iteration,a,b) +   # for volatility, wf_iteration_ar1 is stated above
         rho*y_ar1[i-1] + u[i]
     }
   }
@@ -212,10 +218,9 @@ for(i in 1:n)
       trend_ar1[i] <- wf(x[i],1,a,b) +
         rho*trend_ar1[i-1]
     }
-    
     else
     {
-      trend_ar1[i] <- wf(x[i],wf_iteration_ar1,a,b) +
+      trend_ar1[i] <- wf(x[i],wf_iteration,a,b) +
         rho*trend_ar1[i-1]
     }
   }
@@ -224,10 +229,22 @@ for(i in 1:n)
 # The plots illustrates the time-series and its autoregressive trend
                   
 plot(x,y_ar1,type="l",                                # the plot of the time-series
+     xlab = 't',
      ylim = c(min(min(trend_ar1), min(y_ar1))-stdev,
               max(max(trend_ar1), max(y_ar1))+stdev)
 )
 lines(x,trend_ar1,col="red")                          # the plot of the trend
+
+
+
+
+
+
+
+
+
+
+
 
 # Variance tests of the differenced for of the time-series within the volatile vs stable periods
 
